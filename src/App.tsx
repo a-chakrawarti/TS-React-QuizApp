5ffcaf6c2cb605d6f06ce79efd3aa3a695e827d1
CURVX <chakrawarti.anupam@gmail.com>
2021-05-10 19:16:25 +0530

    Completed QuizApp TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import QuestionCard from "./components/QuestionCard";
+import { fetchQuizQuestions, QuestionState, Difficulty } from "./API";
+import { GlobalStyle, Wrapper } from "./App.styles";
+export type MouseEvent = React.MouseEvent<HTMLButtonElement>;
 
-function App() {
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [isGameOver, setIsGameOver] = useState(true);
+
+  // console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+  // console.log(questions);
+  // Try-catch
+  const startTrivia = async () => {
+    setIsLoading(true);
+    setIsGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setQuestionNumber(0);
+    setIsLoading(false);
+  };
+
+  const checkAnswers = (e: MouseEvent) => {
+    if (!isGameOver) {
+      const answer = e.currentTarget.value;
+      const correct = questions[questionNumber].correct_answer === answer;
+
+      if (correct) {
+        setScore((prevScore) => prevScore + 1);
+      }
+      const answerObject = {
+        question: questions[questionNumber].question,
+        answer,
+        correct,
+        correctAnswer: questions[questionNumber].correct_answer,
+      };
+
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
+
+  const nextQuestion = () => {
+    const nextQuestion = questionNumber + 1;
+
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setIsGameOver(true);
+    } else {
+      setQuestionNumber(nextQuestion);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <GlobalStyle />
+      <Wrapper>
+        <h1>React Quiz</h1>
+        {isGameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button className="start" onClick={startTrivia}>
+            Start
+          </button>
+        ) : null}
+        {!isGameOver && <p className="score">Score: {score}</p>}
+        {isLoading && <p>Loading Question...</p>}
+        {!isLoading && !isGameOver && (
+          <QuestionCard
+            questionNumber={questionNumber + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[questionNumber].question}
+            answers={questions[questionNumber].answers}
+            userAnswer={userAnswers ? userAnswers[questionNumber] : undefined}
+            callback={checkAnswers}
+          />
+        )}
+        {!isGameOver &&
+        !isLoading &&
+        userAnswers.length === questionNumber + 1 &&
+        questionNumber !== TOTAL_QUESTIONS - 1 ? (
+          <button className="next" onClick={nextQuestion}>
+            Next Question
+          </button>
+        ) : null}
+      </Wrapper>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
